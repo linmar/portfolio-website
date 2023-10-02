cac593108ac5f62dbbea36a98a420cc3ae9928d9
Markus Linke <markus.linke@linke.de>
2023-10-02 11:39:12 +0700

    typo corrected

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,21 @@ import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
+import IntroLoggedIn from "@/components/introLoggedIn";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 import UserProfile from "@/components/UserProfile";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
+export default async function Home() {
   const { getUser, isAuthenticated } = getKindeServerSession();
   const user = await getUser();
   const loggedIn = await isAuthenticated();
 
   return (
     <main className="flex flex-col items-center px-4">
-      loggedIn ?
-      <IntroLoggedIn /> : <Intro />
+      {(await isAuthenticated()) ? <IntroLoggedIn /> : <Intro />}
       <SectionDivider />
       <About />
       <Projects />
