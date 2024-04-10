26b37ec6cc68945df2101acb9890e66a69cc5902
Markus Linke <markus.linke@linke.de>
2024-04-10 11:11:35 +0700

    2024 updates

--- components/intro.tsx
+++ components/intro.tsx
@@ -27,7 +27,8 @@ export default function Intro() {
             }}>
             <Image
               // src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-              src="https://res.cloudinary.com/ddnfxz7wl/image/upload/q_auto/f_auto/w_368/h_368/v1696167175/Linke.de/markus.jpg"
+              //src="https://res.cloudinary.com/ddnfxz7wl/image/upload/q_auto/f_auto/w_368/h_368/v1696167175/Linke.de/markus.jpg"
+              src="https://res.cloudinary.com/ddnfxz7wl/image/upload/q_auto/f_auto/w_368/h_368/v1712722188/Linke.de/markus.png"
               alt="Markus portrait"
               width="192"
               height="192"
